578cef8ea4db9fc727b39e445b96f7582870878a
MacVincent Agha-Oko <macvincentaghaoko@gmail.com>
2019-05-07 21:42:27 +0000

    Updated RouteModel variables and methods

--- src/route_model.h
+++ src/route_model.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef ROUTE_MODEL_H
+#define ROUTE_MODEL_H
 
 #include <limits>
 #include <cmath>
@@ -15,18 +16,31 @@ class RouteModel : public Model {
         
         Node(){}
         Node(int idx, RouteModel * search_model, Model::Node node) : Model::Node(node), parent_model(search_model), index(idx) {}
-      
+        Node* parent = nullptr;
+        float h_value = std::numeric_limits<float>::max();
+        float g_value = 0.0;
+        bool visited = false;
+        std::vector<Node*> neighbors;
+        double distance (Node);
+        void FindNeighbors();
       private:
         // Add private Node variables and methods here.
         int index;
+        Node* FindNeighbor(std::vector<int> node_indicies);
         RouteModel * parent_model = nullptr;
     };
     
     // Add public RouteModel variables and methods here.
     RouteModel(const std::vector<std::byte> &xml);  
     std::vector<Node> path; // This variable will eventually store the path that is found by the A* search.
-
+	  std::vector<Node>& SNodes();
+    std::unordered_map<int, std::vector<Model::Road*>> getNodeToRoadHashmap();
+    Node& FindClosestNode(double, double);
   private:
     // Add private RouteModel variables and methods here.
-
+  	std::vector<Node> mNodes;
+    std::unordered_map<int, std::vector< Model::Road*>>node_to_road;
+    void CreateNodeToRoadHashmap();
 };
+
+#endif
